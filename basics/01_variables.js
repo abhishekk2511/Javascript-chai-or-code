60e8dcff4e245b29326f32d9429dcedd85100eeb
abhishekk2511 <abhishekhakmak2@gmail.com>
2025-10-22 19:26:14 +0530

    push

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_id = 23134
+let account_email = "abhishek@gmail.com"
+var account_password = "12345"
+account_city = "Delhi"
+let account_state;
+
+// account_id = 2
+// prefer not to use var keyword
+
+account_email = "sonu@gmail.com"
+account_password = "21211121"
+account_city = "jaipur"
+
+console.log(account_id)
+
+console.table([account_id, account_email, account_password, account_city, account_state])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all js version as a newer version of javascript
+
+// alert(3+3); //used in browser console
+
+console.log(3+3)
+console.log("Abhishek")
+
+
+let age = 22
+let names = "abhishek"
+let isLoggedIn = false;
+let state;
+
+//number => range is 2^53
+// for bigger number = bigint
+// string = "" 
+// Boolean = true/false
+// null => stand alone value
+// undefined =>
+// symbol => used for unique
+
+// Object => vast topic, furthur videos hai ispe 
+
+console.log(typeof null);
+console.log(typeof age);
+console.log(typeof names);  
+console.log(typeof undefined);
