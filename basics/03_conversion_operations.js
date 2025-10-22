9cb69b48f0580e2b095b20d95d635853b17cadf8
abhishekk2511 <abhishekhakmak2@gmail.com>
2025-10-22 21:28:25 +0530

    push

---
+++ basics/03_conversion_operations.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+// conversion
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33 
+// "33abc" => NaN(not a number)
+// true => 1, false => 0
+
+let isLoggedIn = 1;
+
+// conversion
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn);
+
+// 1 => true , 0 => false
+//"" => false
+// "abhishek" => true
+
+let someNumber = 33
+console.log(someNumber)
+
+let StringNumber = String(someNumber);
+console.log(typeof StringNumber)
+console.log(StringNumber)
