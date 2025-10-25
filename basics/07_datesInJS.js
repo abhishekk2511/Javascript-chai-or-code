e3f50fce9e42fa5edbc154dedafeef8e9cc33145
abhishekk2511 <abhishekhakmak2@gmail.com>
2025-10-25 17:41:09 +0530

    --

---
+++ basics/07_datesInJS.js
@@ -0,0 +1,28 @@
+// DATES
+
+let myDate = new Date();
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+
+// let myCreatedDate = new Date("2002-11-25")
+
+// indian Date format -> MM-DD-YYYY
+let myCreatedDate = new Date("11-25-2002")
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleString()); 
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate.toDateString());  //gives todays date
+
+
+
