957750adc1ec586dc6e54de86797654bf8a0c453
abhishekk2511 <abhishekhakmak2@gmail.com>
2025-10-27 06:38:00 +0530

    push

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,32 @@
+
+function sayMyName () {
+    console.log("A");
+    console.log("b");
+    console.log("h");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+    console.log("e");
+    console.log("k");    
+}
+
+// sayMyName()
+
+function addTwoNumber (num1, num2){
+    // console.log(m1 + num2);
+    return num1+num2
+}
+let result = addTwoNumber(3, 3)
+// console.log(result);
+
+function loginuserName(userName){
+    if(!undefined){
+        console.log("Please enter a username");
+        // return
+    }
+    return `${userName} just logged in` 
+}
+// loginuserName("abhishek")
+// console.log(loginuserName("abhishek"));
+
+
